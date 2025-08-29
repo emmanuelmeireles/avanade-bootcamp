7708ce761290a754aea84ad03da480614ee2c550
Emmanuel Meireles Silva <emmanuelmeireles@gmail.com>
2025-08-29 20:40:52 +0000

    estou voltando aos poucos

---
+++ MeuProjeto/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+namespace MyProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("digite o numero:");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        int c = a + b;
+
+
+        Console.WriteLine("o resultado é " + c);
+    }
+}*/
+
